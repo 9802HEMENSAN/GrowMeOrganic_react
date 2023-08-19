6eb978e0a69d2de5d95c1cd81a18bf861e0fa4a4
9802HEMENSAN <hmahilange9802@gmail.com>
2023-08-19 10:40:51 +0530

    React assignment done

---
+++ vite-project/src/components/Navbar.tsx
@@ -0,0 +1,49 @@
+import { Link as RouterLink, useNavigate } from "react-router-dom";
+import { AppBar, Toolbar, Typography, Link,Button } from '@mui/material';
+
+const styles = {
+    nav : {
+        position : "sticky",
+        top : "0px",
+        backgroundColor : "black",
+        ZIndex : 1000,
+        
+    },
+    navbar: { 
+        display : "flex",
+      flexGrow: 1,   
+      justifyContent: "space-evenly"
+    },
+    navItem: {
+      marginLeft: '20px',
+      textDecoration: 'none',
+      color: 'white',
+    },
+  };
+  
+const Navbar = () => {
+    const navigate = useNavigate()
+    const LogOut = ()=> {
+        localStorage.removeItem("userData");
+        navigate("/")
+    }
+    
+  return (
+    <AppBar position="static" sx={styles.nav }>
+    <Toolbar>
+      <Typography variant="h6" component="div" sx={styles.navbar}>
+        <Link component={RouterLink} to="/" sx={styles.navItem}>
+          Login
+        </Link>
+        <Link component={RouterLink} to="/data" sx={styles.navItem}>
+          Blog
+        </Link>
+
+        <Button variant="contained" onClick={LogOut}>LogOut</Button>
+      </Typography>
+    </Toolbar>
+  </AppBar>
+  );
+};
+
+export default Navbar;
